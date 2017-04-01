2bfb432da2e10ade2a0f91d9c5f2907b26d04006
okasho <admin@okamu.ro>
2017-04-01 16:13:25 +0900

    Initial commit

---
+++ YearApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YearApp
+//
+//  Created by shogo okamuro on 2017/04/01.
+//  Copyright © 2017 ro.okamu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
